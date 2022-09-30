734294406aa1f72ff97291ef5d9ab6e5364786b2
chae0 <chae0_98@naver.com>
2022-09-30 11:11:08 +0900

    Version 1.2.0 20220930 chae0 Try to SignUp. How to set a Korean KeyBoard?

---
+++ app/src/main/java/com/example/smarthome0807/Fragment3.java
@@ -0,0 +1,50 @@
+package com.example.smarthome0807;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Fragment3 extends Fragment {
+    ViewGroup viewGroup;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_3,container,false);
+
+        return viewGroup;
+    }
+}
---
+++ app/src/main/java/com/example/smarthome0807/UserData.java
@@ -0,0 +1,11 @@
+package com.example.smarthome0807;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UserData {
+    @GET("/api/users/getUsers")
+    Call<Map<String,String>> getPosts();
+}
