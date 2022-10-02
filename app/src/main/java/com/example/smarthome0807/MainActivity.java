fc52a0b727a8152870112ab19ff61464bf39338c
Qnokka <soly0911@naver.com>
2022-10-02 19:45:26 +0900

    Version 1.2.7 Change fragment order

--- app/src/main/java/com/example/smarthome0807/MainActivity.java
+++ app/src/main/java/com/example/smarthome0807/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         fragment3 = new Fragment_settings();
 
         //뷰 세팅
-        getSupportFragmentManager().beginTransaction().replace(R.id.frag_frame, fragment1).commitAllowingStateLoss();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_frame, fragment2).commitAllowingStateLoss();
         //bottomnavigationview의 아이콘을 선택 했을때 원하는 프래그먼트가 띄워질 수 있도록 리스너 추가
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -38,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
                     //menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.
                     case R.id.tab1: {
                         getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.frag_frame, fragment1).commitAllowingStateLoss();
+                                .replace(R.id.frag_frame, fragment2).commitAllowingStateLoss();
                         return true;
                     }
 
                     case R.id.tab2: {
                         getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.frag_frame, fragment2).commitAllowingStateLoss();
+                                .replace(R.id.frag_frame, fragment1).commitAllowingStateLoss();
                         return true;
                     }
 
